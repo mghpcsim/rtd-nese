8ac28276656e6989e7b93a51219732a4018c8bc3
Christopher S. Simmons <csim@mghpcc.org>
2023-03-12 17:07:59 -0400

    updating conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Project NESE'
+copyright = '2023, Team NESE'
+author = 'csim'
 
 release = '0.1'
 version = '0.1.0'
